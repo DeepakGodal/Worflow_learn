74fecc966fa1f994b901595394a66a9ddeaebb33
DeepakGodal <161459617+DeepakGodal@users.noreply.github.com>
2026-02-05 12:30:36 +0530

    Sales table creation

---
+++ 010_create_table_sales.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE TABLE SALES_RAW (
+  TRANSACTION_ID         NUMBER,
+  STORE_ID               NUMBER,
+  PRODUCT_ID             NUMBER,
+  CUSTOMER_ID            NUMBER,
+  QUANTITY               NUMBER,
+  UNIT_PRICE             NUMBER(10,2),
+  DISCOUNT               NUMBER(5,2),
+  SALES_TIMESTAMP        TIMESTAMP_NTZ,
+
+  -- Ingestion metadata
+  _LOAD_TS               TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP(),
+  _SOURCE_FILE           STRING,
+  _ROW_NUMBER            NUMBER
+);
